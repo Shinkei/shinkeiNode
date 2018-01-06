a1ad654f02032ae7e6fd4045465c1ee48025d397
Shinkei <shinkeygentatsu@yahoo.com.mx>
2018-01-06 18:05:53 -0500

    add route to see the get params and ignore environment files

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,15 @@ router.get('/', (req, res) => {
   res.send('Hey! It works!');
 });
 
+router.get('/echoed', (req, res) => {
+  if(Object.keys(req.query).length === 0 ){
+    res.send('try to send parameters in the url, eg: echoed?param1=value1&param2=value2');
+  }
+  else{
+    res.send(req.query);
+  }
+});
+
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
   res.send(reverse);
