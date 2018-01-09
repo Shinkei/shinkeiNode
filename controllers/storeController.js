2d8cc2088575cf6ae673880041bb0b36c2c95289
shinkei <ing.jorgeivanramirezh@gmail.com>
2018-01-09 11:04:09 -0500

    organiza the functions that are used in the routes ans send them to the controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,19 @@
 exports.homePage = (req, res) => {
   res.render('index', {name:'perro', title: 'home'});
 };
+
+// this function has the porpuse to show how to get values fron the get url
+exports.echoed = (req, res) => {
+  if(Object.keys(req.query).length === 0 ){
+    res.send('try to send parameters in the url, eg: echoed?param1=value1&param2=value2');
+  }
+  else{
+    res.send(req.query);
+  }
+};
+
+// this function reverse the given name in the url
+exports.reverse_name = (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+};
--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,8 @@ const storeController = require('../controllers/storeController');
 // Do work here
 router.get('/', storeController.homePage);
 
-router.get('/echoed', (req, res) => {
-  if(Object.keys(req.query).length === 0 ){
-    res.send('try to send parameters in the url, eg: echoed?param1=value1&param2=value2');
-  }
-  else{
-    res.send(req.query);
-  }
-});
+router.get('/echoed', storeController.echoed);
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.get('/reverse/:name', storeController.reverse_name);
 
 module.exports = router;
