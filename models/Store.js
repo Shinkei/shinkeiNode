32fa32ce11b2fa3ea1f1ee19fa9ccb78e097225b
Shinkei <shinkeygentatsu@yahoo.com.mx>
2018-01-13 22:56:48 -0500

    add new fields for address, longitude and latitude and put the inputs in the form

--- models/Store.js
+++ models/Store.js
@@ -5,7 +5,7 @@ const slug = require('slugs');
 
 const storeSchema = new mongoose.Schema({
     name: {
-        type:String,
+        type: String,
         trim: true,
         required: 'Please enter a store name'
     },
@@ -14,12 +14,30 @@ const storeSchema = new mongoose.Schema({
         type: String,
         trim: true
     },
-    tags: [String]
+    tags: [String],
+    created: {
+        type: Date,
+        default: Date.now()
+    },
+    location: {
+        type: {
+            type: String,
+            default: 'Point'
+        },
+        coordinates: [{
+            type: Number,
+            required: 'you must supply coordinates'
+        }],
+        address: {
+            type: String,
+            required: 'you must supply an address'
+        }
+    }
 });
 
 // this function will execute every time before every save
-storeSchema.pre('save', function(next){
-    if(!this.isModified('name')){
+storeSchema.pre('save', function (next) {
+    if (!this.isModified('name')) {
         next();
         return;
     }
