4fc8c795109826319ce59033bf4b61acc5518703
shinkei <ing.jorgeivanramirezh@gmail.com>
2018-01-22 16:01:54 -0500

    add account page that allows to update the user data

--- routes/index.js
+++ routes/index.js
@@ -13,13 +13,17 @@ router.post('/add', storeController.upload, catchErrors(storeController.resize),
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.post('/add/:id', storeController.upload, catchErrors(storeController.resize), catchErrors(storeController.updateStore)); // this is a handler that will wrap the funtion to catch the errors
 router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
+
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
 router.get('/register', userController.registerForm);
 router.post('/register', userController.validateRegister, userController.register, authController.login);
 router.get('/login', userController.loginForm);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', authController.isLoggedIn, catchErrors(userController.updateAccount));
 
 router.get('/echoed', storeController.echoed);
 router.get('/reverse/:name', storeController.reverse_name);
