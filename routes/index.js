4cfa3ec074e661cd879b028103712547018c747c
shinkei <ing.jorgeivanramirezh@gmail.com>
2018-01-22 15:30:41 -0500

    add validation to check that an user is loggedin and implement the logout function and the gravatar picture

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,18 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 router.get('/', storeController.homePage);
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 router.post('/add', storeController.upload, catchErrors(storeController.resize), catchErrors(storeController.createStore)); // this is a handler that will wrap the funtion to catch the errors
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.post('/add/:id', storeController.upload, catchErrors(storeController.resize), catchErrors(storeController.updateStore)); // this is a handler that will wrap the funtion to catch the errors
 router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+router.get('/register', userController.registerForm);
+router.post('/register', userController.validateRegister, userController.register, authController.login);
 router.get('/login', userController.loginForm);
-router.post('/login', userController.loginForm);
-router.get('/register',userController.registerForm);
-router.post('/register',userController.validateRegister, userController.register, authController.login);
-
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 
 router.get('/echoed', storeController.echoed);
 router.get('/reverse/:name', storeController.reverse_name);
